42220245132475bcf044f407aa690da6d1230ae0
Joel Armando <joelarma@gmail.com>
2014-11-17 09:07:18 +0000

    Add FunctionalLinkedList implementation and tests

---
+++ FunctionalLinkedListTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class FunctionalLinkedListTest extends  FunctionalListTest {
+
+	public FunctionalList getEmptyList() {
+		return new FunctionalLinkedList();
+	}
+}
