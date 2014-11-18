6f66b237deb35e661f8998281e7af99cd41d607d
Joel Armando <joelarma@gmail.com>
2014-11-18 23:16:55 +0000

    Add implementation and tests for ImprovedStack

---
+++ ImprovedStackImpl.java
@@ -0,0 +1,115 @@
+/**
+ * An implementation of a stack with additional methods. 
+ * 
+ * Classes implementing this interface must use a {@see List} as the
+ * underlying data structure to store the elements on the stack. 
+ * 
+ * Not all operations on a stack will always be successful. For
+ * example, a programmer may try to pop an element from an empty
+ * stack. Since we hace not covered exceptions yet, we need another
+ * mechanism to report errors. In order to do that, methods of this
+ * list will return a {@see ReturnObject} that will contain either an
+ * object or an error value of the right kind (as defined in {@see
+ * ErrorMessage}).
+ * 
+ * @author Joel Armando
+ */
+public class ImprovedStackImpl implements ImprovedStack {
+	protected List internalList;
+
+	public ImprovedStackImpl(List list) {
+		this.internalList = list;
+	}
+
+	public ImprovedStackImpl() {
+		this(new LinkedList());
+	}
+
+	/**
+	 * Returns true if the stack is empty, false otherwise. 
+	 * 
+	 * @return true if the stack is empty, false otherwise. 
+	 */
+	public boolean isEmpty() {
+		return internalList.isEmpty();
+	}
+
+	/**
+	 * Returns the number of items currently in the stack.
+	 * 
+	 * @return the number of items currently in the stack
+	 */
+	public int size() {
+		return internalList.size();
+	}
+
+	/**
+	 * Adds an element at the top of the stack. 
+	 * 
+	 * @param item the new item to be added
+	 */
+	public void push(Object item) {
+		internalList.add(0, item);
+	}
+
+	/**
+	 * Returns the element at the top of the stack. The stack is
+	 * left unchanged.
+	 * 
+	 * @return If stack is not empty, the item on the top is returned. If the
+	 *         stack is empty, an appropriate error.
+	 */
+	public ReturnObject top() {
+		return internalList.get(0);
+	}
+
+	/**
+	 * Returns the element at the top of the stack. The element is
+	 * removed frmo the stack.
+	 * 
+	 * @return If stack is not empty, the item on the top is returned. If the
+	 *         stack is empty, an appropriate error.
+	 */
+	public ReturnObject pop() {
+		return internalList.remove(0);
+	}
+
+	/**
+	 * Returns a copy of this stack with the items reversed, the top
+	 * elements on the original stack is at the bottom of the new
+	 * stack and viceversa.
+	 * 
+	 * @return a copy of this stack with the items reversed. 
+	 */
+	public ImprovedStack reverse() {
+		ImprovedStack newStack = new ImprovedStackImpl();
+		int i = 0;
+		while (i < internalList.size()) {
+			newStack.push(internalList.get(i).getReturnValue());
+			i++;
+		}
+		return newStack;
+	}
+
+	/**
+	 * Removes the given object from the stack if it is
+	 * there. Multiple instances of the object are all removed.
+	 *
+	 * Classes implementing this method must use method .equals() to
+	 * check whether the item is in the stack or not.
+	 * 
+	 * @param object the object to remove
+	 */
+	public void remove(Object object) {
+		int i = 0;
+		while (i < internalList.size()) {
+			if (object.equals(internalList.get(i).getReturnValue())) {
+				internalList.remove(i);
+			} else {
+				i++;
+			}
+		}
+	}
+		
+}
+
---
+++ ImprovedStackImplTest.java
@@ -0,0 +1,43 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ImprovedStackImplTest extends StackTest {
+
+	public Stack getEmptyStack() {
+		return new ImprovedStackImpl();
+	}
+
+	@Test 
+	public void testReverse() {
+		ImprovedStack oldStack = (ImprovedStackImpl)getEmptyStack();
+		oldStack.push(new Integer(1));
+		oldStack.push(new Integer(2));
+		oldStack.push(new Integer(3));
+		ImprovedStack newStack = oldStack.reverse();
+		assertEquals(3, newStack.size());
+		assertEquals(3, oldStack.size());
+		assertEquals(1, newStack.pop().getReturnValue());
+		assertEquals(2, newStack.pop().getReturnValue());
+		assertEquals(3, newStack.pop().getReturnValue());
+		assertEquals(3, oldStack.pop().getReturnValue());
+		assertEquals(2, oldStack.pop().getReturnValue());
+		assertEquals(1, oldStack.pop().getReturnValue());
+
+	}
+
+	@Test
+	public void testRemove() {
+		ImprovedStack oldStack = (ImprovedStackImpl)getEmptyStack();
+		oldStack.push(new Integer(1));
+		oldStack.push(new Integer(2));
+		oldStack.push(new Integer(1));
+		oldStack.push(new Integer(1));
+		oldStack.push(new Integer(3));
+		oldStack.push(new Integer(1));
+		oldStack.remove(new Integer(1));
+		assertEquals(2, oldStack.size());
+		assertEquals(3, oldStack.pop().getReturnValue());
+		assertEquals(2, oldStack.pop().getReturnValue());
+	}
+
+}
